5b15a3759974c42e77925ef6128af5b3deda3336
ANTOND <71350868+antond15@users.noreply.github.com>
2023-04-11 15:26:23 +0200

    fix(web/row): badge element

--- web/src/components/Row.tsx
+++ web/src/components/Row.tsx
@@ -1,14 +1,26 @@
-import { Badge, Group, Text } from '@mantine/core';
+import { Group, Text, createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  badge: {
+    backgroundColor: theme.colors.gray[9],
+    height: '20px',
+    padding: '0 10px',
+    borderRadius: theme.radius.xs,
+    fontSize: theme.fontSizes.xs,
+    fontWeight: 700,
+    lineHeight: '18px',
+  },
+}));
 
 const Row: React.FC = () => {
+  const { classes } = useStyles();
+
   return (
     <Group bg="dark.8" p={5} position="apart" spacing={0} noWrap>
       <Text lineClamp={1} weight={600}>
         ANTOND. 4ds65 4g5e s64 g65 es4 g6 s4e 65g4
       </Text>
-      <Badge color="gray" radius="xs" size="md">
-        6
-      </Badge>
+      <span className={classes.badge}>6</span>
     </Group>
   );
 };
