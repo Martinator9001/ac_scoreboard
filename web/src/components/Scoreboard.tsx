164c704dbd114df548cee93590ff45ef04ad5e96
ANTOND <71350868+antond15@users.noreply.github.com>
2023-04-24 15:32:52 +0200

    fix(web/drawer): incorrect title order

--- web/src/components/Scoreboard.tsx
+++ web/src/components/Scoreboard.tsx
@@ -1,20 +1,30 @@
-import { Drawer, Stack, Title } from '@mantine/core';
+import { Drawer, Stack, createStyles } from '@mantine/core';
 import { useVisibility } from '../store/visibility';
 import Row from './Row';
 import Footer from './footer';
 
+const useStyles = createStyles({
+  title: {
+    fontSize: '1.375rem',
+    fontWeight: 700,
+    lineHeight: 1.4,
+  },
+});
+
 const Scoreboard: React.FC = () => {
+  const { classes } = useStyles();
   const [visible, setVisible] = useVisibility((state) => [state.visible, state.setVisible]);
 
   return (
     <Drawer
+      classNames={{ title: classes.title }}
       opened={visible}
       onClose={() => setVisible(false)}
       withCloseButton={false}
       size="xs"
       position="right"
       padding="sm"
-      title={<Title order={3}>Server Name</Title>}
+      title="Server Name"
     >
       <Stack align="center">
         <Stack spacing={5}>
